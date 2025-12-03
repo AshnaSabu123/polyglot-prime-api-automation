d03ba04820c0ada67655d998f05dfcdb1ab108d9
AshnaSabu123 <97601313+AshnaSabu123@users.noreply.github.com>
2025-12-03 10:23:34 +0530

    Update playwright.config.ts

--- api-automation/playwright.config.ts
+++ api-automation/playwright.config.ts
@@ -4,7 +4,7 @@ export default defineConfig({
   testDir: './tests',
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
-  retries: process.env.CI ? 1 : 0,
+  retries: process.env.CI ? 0 : 0,
   workers: process.env.CI ? 1 : 1,
   timeout: 60000, // 60 seconds for each test
   reporter: 'html',
